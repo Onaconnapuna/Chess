6b08bdc8c92ff594d03c15a83778c810e55a9119
Onaconnapuna <cjgermain12@gmail.com>
2022-11-17 18:52:01 -0500

    added deltas to slideable pieces

--- pieces/bishop.cpp
+++ pieces/bishop.cpp
@@ -3,5 +3,7 @@
 
 BishopPiece::BishopPiece(int posX, int posY, std::string add_color) : Piece(posX, posY) {
   value = "bishop";
+  slideable = true;
   color = add_color;
+  deltas = { { -1, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 } };
 }
--- pieces/queen.cpp
+++ pieces/queen.cpp
@@ -4,4 +4,6 @@
 QueenPiece::QueenPiece(int posX, int posY, std::string add_color) : Piece(posX, posY) {
   value = "queen";
   color = add_color;
+  slideable = true;
+  deltas = { {-1, 0}, {0, 1}, {1, 0}, {-1, 0},{ -1, 1 }, { 1, 1 }, { -1, -1 }, { 1, -1 } };
 }
